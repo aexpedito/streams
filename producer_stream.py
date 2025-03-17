1af15f1b59358161e0b84d98d611fc769aabaccd
aexpedito <aexpedito@hotmail.com>
2025-03-17 17:31:15 -0300

    Stream commited

--- producer_stream.py
+++ producer_stream.py
@@ -1,11 +1,20 @@
-import pandas as pd
+from sys import api_version
 
+from kafka import KafkaProducer
+import pandas as pd
 
 
 def main():
 	print("Starting producer")
 	data = pd.read_csv("creditCardData/card_transdata.csv")
 	print(data.info())
+	producer = KafkaProducer(bootstrap_servers='localhost:9092', api_version)
+
+	producer.send('test_topic',b'Hi!')
+
+	producer.flush()
+
+	print("Finished")
 
 if __name__ == '__main__':
 	main()
